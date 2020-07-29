a39397da6ed5c5c0089b4a0453405b556fa17c60
Kyle Martinez <63060972+kymarti@users.noreply.github.com>
2020-07-29 14:12:15 -0400

    Updated setup.py

---
+++ lib/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='core',
+      version='0.1',
+      description='Core library used by notebooks in this repo.',
+      packages=['core'],
+      zip_safe=False)
